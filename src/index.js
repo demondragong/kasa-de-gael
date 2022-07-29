982e960aa986533e7311d66b911892e0fa9204b5
Gael <demondragong@gmail.com>
2022-07-29 17:56:12 +0200

    add routing with react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from './routes/About'
+import Error from './routes/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="a-propos" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Route>  
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/routes/Logement.jsx
@@ -0,0 +1,5 @@
+export default function Logement() {
+    return (
+        "Welcome logeme"
+    )
+}
